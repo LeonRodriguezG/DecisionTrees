a4a3f99ae0aae2ce7d4bd45774f2737a41e2f544
LeonRodriguezG <a01700687@itesm.mx>
2018-10-23 17:52:11 -0500

    1sr Parse

--- run.rb
+++ run.rb
@@ -4,23 +4,22 @@ newLine=nil
 allAtt=[]
 dataSet=[]
 auxLine=nil
- 
-lines = $stdin.readlines
+
 
 lineIndex=0
 
 while(relation.split('\ ')[0]!= "@relation")
-  relation = lines[lineIndex+=1]
+  relation = gets.chomp
 end
 relation = relation.split('\ ')[1]
 
 while(newLine.split('\ ')[0]!= "@attribute")
-  newLine = lines[lineIndex+=1]
+  newLine = gets.chomp
   while(newLine.split('\ ')[0]=="@attribute")
     allAtt[0].push(newLine.split('\ ')[1])
     allAtt[1].push(newLine.split('{')[1].gsub('\ ','').gsub('}','').split(','))
     auxLine=newLine
-    newLine = lines[lineIndex+=1]
+    newLine = gets.chomp
   end
   newLine=auxLine if auxLine.split('\ ')[0]== "@attribute"
 end
@@ -28,7 +27,7 @@ end
 while(newLine.split('\ ')[0]!= "@data")
   newLine = lines[lineIndex+=1]
 end
-newLine = lines[lineIndex+=1]
+newLine = gets.chomp
 while newLine != nil
   if newLine[0] =! '%'
     dataSet.push(newLine.split(','))

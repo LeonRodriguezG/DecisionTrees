0f39671cb3e784ea1433e61c3f347993d045722f
Juan Ledesma <a01206568@itesm.mx>
2018-10-23 18:08:35 -0500

    Test1

--- run.rb
+++ run.rb
@@ -6,32 +6,33 @@ dataSet=[]
 auxLine=nil
 
 #Por favor 
-
-while(relation.split('\ ')[0]!= "@relation")
+relation = gets.chomp
+while(relation.split(' ')[0]!= "@relation")
   relation = gets.chomp
 end
-relation = relation.split('\ ')[1]
+relation = relation.split(' ')[1]
 
-while(newLine.split('\ ')[0]!= "@attribute")
+newLine = gets.chomp
+while(newLine.split(' ')[0]!= "@attribute")
   newLine = gets.chomp
-  while(newLine.split('\ ')[0]=="@attribute")
-    allAtt[0].push(newLine.split('\ ')[1])
-    allAtt[1].push(newLine.split('{')[1].gsub('\ ','').gsub('}','').split(','))
+  while(newLine.split(' ')[0]=="@attribute")
+    allAtt[0].push(newLine.split(' ')[1])
+    allAtt[1].push(newLine.split('{')[1].gsub(/ /,'').gsub(/}/,'').split(','))
     auxLine=newLine
     newLine = gets.chomp
   end
-  newLine=auxLine if auxLine.split('\ ')[0]== "@attribute"
+  newLine=auxLine if auxLine.split(' ')[0] == "@attribute"
 end
 
-while(newLine.split('\ ')[0]!= "@data")
+while(newLine.split(' ')[0]!= "@data")
   newLine = gets.chomp
 end
 newLine = gets.chomp
 while newLine != ""
-  if newLine[0] =! '%'
+  if newLine[0] != '%'
     dataSet.push(newLine.split(','))
   end
-  newLine=auxLine if auxLine.split('\ ')[0]== "@attribute"
+  newLine=auxLine if auxLine.split(' ')[0] == "@attribute"
 end
 
 puts relation

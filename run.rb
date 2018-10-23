5edc59fce096358ef834a89b9a1e7a847be1e50a
Juan Ledesma <a01206568@itesm.mx>
2018-10-23 18:53:16 -0500

    Correcting attributes

--- run.rb
+++ run.rb
@@ -1,33 +1,35 @@
 
 relation=nil
 newLine=nil
-allAtt=[]
+#allAtt=Array.new() {Array.new()}
+allAtt = Hash.new
 dataSet=[]
 auxLine=nil
 
-#Por favor 
 relation = gets.chomp
-while(relation.split(' ')[0]!= "@relation")
+while(relation.split(' ')[0] != "@relation")
   relation = gets.chomp
 end
 relation = relation.split(' ')[1]
+#puts "Relation: #{relation}"
 
 newLine = gets.chomp
-while(newLine.split(' ')[0]!= "@attribute")
+while(newLine.split(' ')[0] != "@attribute")
   newLine = gets.chomp
-  while(newLine.split(' ')[0]=="@attribute")
-    allAtt.push(newLine.split(' ')[1])
-    allAtt[allAtt.length - 1].push(newLine.split('{')[1].gsub(/ /,'').gsub(/}/,'').split(','))
-    auxLine=newLine
+  while(newLine.split(' ')[0] == "@attribute")
+    allAtt[(":" + newLine.split(' ')[1])] = newLine.split('{')[1].gsub(/ /,'').gsub(/}/,'').split(',')
+    #allAtt.push(newLine.split(' ')[1])
+    #puts "AllAtt: #{allAtt}"
+    auxLine = newLine
     newLine = gets.chomp
   end
-  newLine=auxLine if auxLine.split(' ')[0] == "@attribute"
+  newLine = auxLine if auxLine.split(' ')[0] == "@attribute"
 end
 
-while(newLine.split(' ')[0]!= "@data")
+newLine = gets.chomp
+while(newLine.split(' ')[0] != "@data")
   newLine = gets.chomp
 end
-newLine = gets.chomp
 while newLine != ""
   if newLine[0] != '%'
     dataSet.push(newLine.split(','))
@@ -37,3 +39,4 @@ end
 
 puts relation
 puts allAtt
+puts dataSet

caf11d4addd5a2d5eac0e88741de3465f53a3f0c
LeonRodriguezG <a01700687@itesm.mx>
2018-10-28 12:38:09 -0600

    T4

--- run.rb
+++ run.rb
@@ -9,38 +9,3 @@ auxLine=nil
 input = gets
 input = input.split('\n')
 puts input
-
-
-relation = gets.chomp
-while(relation.split(' ')[0] != "@relation")
-  relation = gets.chomp
-end
-relation = relation.split(' ')[1]
-#puts "Relation: #{relation}"
-
-newLine = gets.chomp
-while(newLine.split(' ')[0] != "@attribute")
-  newLine = gets.chomp
-end
-
-while(newLine.split(' ')[0] == "@attribute")
-  allAtt[(":" + newLine.split(' ')[1])] = newLine.split('{')[1].gsub(/ /,'').gsub(/}/,'').split(',')
-  #allAtt.push(newLine.split(' ')[1])
-  #puts "AllAtt: #{allAtt}"
-  newLine = gets.chomp
-end
-
-while(newLine.split(' ')[0] != "@data")
-  newLine = gets.chomp
-end
-
-while newLine.split(' ')[0] != nil
-  if newLine.split(' ')[0] != '%'
-    dataSet.push(newLine.split(','))
-  end
-  newLine = gets.chomp
-end
-
-puts relation
-puts allAtt
-puts dataSet

8497b42fc36c46d47794658be030385a4f03b7af
LeonRodriguezG <a01700687@itesm.mx>
2018-10-28 12:39:23 -0600

    T5

--- run.rb
+++ run.rb
@@ -6,6 +6,36 @@ allAtt = Hash.new
 dataSet=[]
 auxLine=nil
 
-input = gets
-input = input.split('\n')
-puts input
+relation = gets
+while(relation.split(' ')[0] != "@relation")
+  relation = gets
+end
+relation = relation.split(' ')[1]
+#puts "Relation: #{relation}"
+
+newLine = gets
+while(newLine.split(' ')[0] != "@attribute")
+  newLine = gets
+end
+
+while(newLine.split(' ')[0] == "@attribute")
+  allAtt[(":" + newLine.split(' ')[1])] = newLine.split('{')[1].gsub(/ /,'').gsub(/}/,'').split(',')
+  #allAtt.push(newLine.split(' ')[1])
+  #puts "AllAtt: #{allAtt}"
+  newLine = gets
+end
+
+while(newLine.split(' ')[0] != "@data")
+  newLine = gets
+end
+
+while newLine.split(' ')[0] != nil
+  if newLine.split(' ')[0] != '%'
+    dataSet.push(newLine.split(','))
+  end
+  newLine = gets
+end
+
+puts relation
+puts allAtt
+puts dataSet
